d3ce081f1fdd58fe401a0e49d87cb4b23fbb80f0
settifoglio <froma.galuppo@gmail.com>
2019-06-12 13:18:23 -0300

    Vector documentado

--- include/models/dataStructures/Vector.hpp
+++ include/models/dataStructures/Vector.hpp
@@ -5,24 +5,73 @@
 #include <utility>
 using namespace std;
 
+///
+/// @file
+
+///
+/// Classe para operações vetoriais no espaço real de duas dimensões.
 class Vector {
   public:
-	float x, y;
 
+	///
+	/// Coordenada x do vetor.
+	float x;
+
+	///
+	/// Coordenada y do vetor.
+	float y;
+
+	///
+	/// Construtor do vetor zero;
 	Vector();
+
+	///
+	/// Construtor do vetor pelas posições x e y.
 	Vector(float, float);
+
+	///
+	/// Construtor do vetor por par de posições.
 	Vector(pair<float, float>);
+
+	///
+	/// Calcula a norma do vetor.
 	float norm();
+
+	///
+	/// Calcula o ângulo do vetor com o eixo x. 
 	float angle();
+
+	///
+	/// Rotaciona o vetor em um dado ângulo.
 	Vector rotate(float);
+
+	///
+	/// Projeção do vetor sobre um outro vetor.
 	Vector project(Vector);
 };
 
+///
+/// Soma de vetores.
 Vector operator+(const Vector &, const Vector &);
+
+///
+/// Subtração de vetores.
 Vector operator-(const Vector &, const Vector &);
+
+///
+/// Multiplicação de um vetor por um escalar.
 Vector operator*(const Vector &, const float);
-float operator*(const Vector &, const Vector &); // dot product
-float operator%(const Vector &, const Vector &); // cross product
-float operator^(const Vector &, const Vector &); // distance
+
+///
+/// Produto escalar entre vetores.
+float operator*(const Vector &, const Vector &);
+
+///
+/// Produto vetorial entre vetores.
+float operator%(const Vector &, const Vector &);
+
+///
+/// Distância normalizada entre vetores.
+float operator^(const Vector &, const Vector &);
 
 #endif
